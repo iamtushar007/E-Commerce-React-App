e8026ad55e5d636b9dfeaba8a1ef969c4c0093a8
Tushar Gahtori <tushargahtori26@gmail.com>
2023-02-24 17:34:08 +0530

    React Footer Design 24.02.2023

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,7 +1,48 @@
 import React from "react";
+import "./Footer.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div className="top">
+        <div className="item">
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Children</span>
+        </div>
+        <div className="item">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Cookies</span>
+        </div>
+        <div className="item">
+          <h1>About</h1>
+          <span>
+            lajksdf asldfkj aklsdjf kasdj flkj sjdklfg skldfjg lkjsf lkdsjf
+            glksjfd gsklfdgjl lkdfj g lksfd jg skldfj sdkfjg lksjdf g kdfjg
+            lksjfdg lksjdf g
+          </span>
+        </div>
+        <div className="item">
+          <h1>Contact</h1>
+          <span>
+            lajksdf asldfkj aklsdjf kasdj flkj sjdklfg skldfjg lkjsf lkdsjf
+            glksjfd gsklfdgjl lkdfj g lksfd jg skldfj sdkfjg lksjdf g kdfjg
+            lksjfdg lksjdf g
+          </span>
+        </div>
+      </div>
+      <div className="bottom">
+        <div className="left">
+          <span className="logo">Laxmi Creations</span>
+        </div>
+        <div className="right">
+          <span className="copy">Copyright 2023.All Rights Reserved</span>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
