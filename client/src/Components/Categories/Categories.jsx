1fa955a51032fe3d723c9a87a73913df5c807526
Tushar Gahtori <tushargahtori26@gmail.com>
2023-03-02 18:21:30 +0530

    Grid System using CSS Flexbox

---
+++ client/src/Components/Categories/Categories.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import "./Categories.scss";
+import { Link } from "react-router-dom";
+const Categories = () => {
+  return (
+    <div className="categories">
+      <div className="col">
+        <div className="row">
+          <img
+            src="https://images.pexels.com/photos/818992/pexels-photo-818992.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          />
+          <button>
+            <Link className="link" to="/products/1">
+              Sale
+            </Link>
+          </button>
+        </div>
+        <div className="row">
+          <img
+            src="https://images.pexels.com/photos/2036646/pexels-photo-2036646.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          />
+          <button>
+            <Link to=".products/2" className="link">
+              Women
+            </Link>
+          </button>
+        </div>
+      </div>
+      <div className="col">
+        <div className="row">
+          <img
+            src="https://images.pexels.com/photos/1813947/pexels-photo-1813947.jpeg?auto=compress&cs=tinysrgb&w=1600"
+            alt=""
+          />
+          <button>
+            <Link className="link" to="/products/3">
+              Men
+            </Link>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
